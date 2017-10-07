f59c72415f7e5e0436e53cce304d237528e5198a
Andy Levy <balagan200@yahoo.co.uk>
2017-10-07 16:40:15 +0100

    Add comments

--- StlMap/StlMap/Source.cpp
+++ StlMap/StlMap/Source.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main() {
 
+	// declare map with int key & string value pair
 	map<int, string> foo;
 
+	// add values
 	foo[93] = "Neptunium";
 	foo[94] = "Plutonium";
 	foo[95] = "Americium";
 	foo[96] = "Curium";
 
+	// print key/value pairs
 	for (map<int, string>::iterator i = foo.begin(); i != foo.end(); ++i)
 		cout << i->first << " - " << i->second << endl;
 
